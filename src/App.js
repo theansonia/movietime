4781a197a804e9f1d555b344a5a77a23a069179e
theansonia <aavellar@users.noreply.github.com>
2021-04-01 19:10:01 -0400

    fixed routitng issue

--- src/App.js
+++ src/App.js
@@ -151,9 +151,9 @@ function App() {
           <Route path={`/details/:name`} component={Details} />
           <Route path={`/moviedetails/:title`} component={MovieDetails} />
         </Switch>
-        <Route path="*"> 
+        {/* <Route path="*"> 
             <Redirect to='/home'/>
-        </Route>
+        </Route> */}
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
