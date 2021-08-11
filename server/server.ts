f23bdf3b8b0da8f7d0852834fad6fcbdfc7de3b2
theansonia <theansonia@users.noreply.github.com>
2021-08-11 14:19:34 -0400

    maybe

---
+++ server/server.ts
@@ -0,0 +1,80 @@
+/* eslint-disable no-console */
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('build'));
+
+app.use('/*', function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// serves index.html at root endpoint
+app.get('/*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+app.get('/*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+app.get('/home', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+app.get('/movies', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+app.get('/shows', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+app.get('/moviedetails/*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+app.get('/tvdetails/*', (req, res) =>
+  res.status(200).sendFile(path.join(__dirname, '../client/index.html'))
+);
+
+/* route handlers */
+
+/* catch all */
+
+app.use('*', (req, res) => res.status(404).send('Oops! Wrong page!'));
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+/* global error handler */
+app.use(
+  (
+    err: express.ErrorRequestHandler,
+    req: express.Request,
+    res: express.Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: express.NextFunction
+  ) => {
+    console.log('got and error', err);
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 500,
+      message: { err: 'Internal Server Error' },
+    };
+
+    const errorObj = Object.assign(defaultErr, err);
+    console.log('Error message from global err handler: ', errorObj.log);
+    return res.status(errorObj.status).send(errorObj.message);
+  }
+);
+
+app.listen(PORT, () => {
+  console.log(`Getting served on port: http://localhost/${PORT}`);
+});
+
+module.exports = app;
