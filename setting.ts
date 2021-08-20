6deee0898a5aef9f85431519b5ce83ab0f7ad15e
theansonia <theansonia@users.noreply.github.com>
2021-08-20 14:39:45 -0400

    added persistent state

---
+++ setting.ts
@@ -0,0 +1,4 @@
+export const postgres =
+  'postgres://yzncynal:LOCFlbNcFXgj8QOptq0VaoncM5yGz42i@chunee.db.elephantsql.com/yzncynal';
+export const SECRET_KEY = 'LeonBlackEnjoysCheese';
+module.exports = { postgres, SECRET_KEY };
